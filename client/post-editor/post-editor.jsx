59ceff054b242f8006073baf0c9a975ee74fc580
Andrew Duthie <andrew@andrewduthie.com>
2016-05-18 14:23:15 -0400

    Editor: Split empty check by state handler

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -50,7 +50,7 @@ import { isEditorDraftsVisible, getEditorPostId } from 'state/ui/editor/selector
 import { toggleEditorDraftsVisible } from 'state/ui/editor/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { receivePost, editPost, resetPostEdits } from 'state/posts/actions';
-import { getPostEdits, isEditedPostDirty, isEditedPostContentEmpty } from 'state/posts/selectors';
+import { getPostEdits, isEditedPostDirty, getEditedPostValue } from 'state/posts/selectors';
 import EditorSidebarHeader from 'post-editor/editor-sidebar/header';
 
 const messages = {
@@ -200,7 +200,7 @@ const PostEditor = React.createClass( {
 			loadingError: PostEditStore.getLoadingError(),
 			isDirty: PostEditStore.isDirty(),
 			isSaveBlocked: PostEditStore.isSaveBlocked(),
-			hasContent: PostEditStore.hasContent(),
+			hasContent: PostEditStore.hasContent( [ 'excerpt', 'content' ] ),
 			previewUrl: PostEditStore.getPreviewUrl(),
 			post: PostEditStore.get(),
 			isNew: PostEditStore.isNew(),
@@ -396,7 +396,7 @@ const PostEditor = React.createClass( {
 								isNew={ this.state.isNew }
 								isDirty={ this.state.isDirty || this.props.dirty }
 								isSaveBlocked={ this.state.isSaveBlocked }
-								hasContent={ this.state.hasContent || ! this.props.postContentEmpty }
+								hasContent={ this.state.hasContent || !! ( this.props.title && this.props.title.trim().length ) }
 								isSaving={ this.state.isSaving }
 								isPublishing={ this.state.isPublishing }
 								onSave={ this.onSave }
@@ -535,7 +535,7 @@ const PostEditor = React.createClass( {
 		actions.edit( edits );
 
 		// Make sure that after TinyMCE processing that the post is still dirty
-		if ( ! PostEditStore.isDirty() || ! PostEditStore.hasContent() || ! this.state.post ) {
+		if ( ! PostEditStore.isDirty() || ! PostEditStore.hasContent( [ 'excerpt', 'content' ] ) || ! this.state.post ) {
 			return;
 		}
 
@@ -888,7 +888,7 @@ export default connect(
 			showDrafts: isEditorDraftsVisible( state ),
 			edits: getPostEdits( state, siteId, postId ),
 			dirty: isEditedPostDirty( state, siteId, postId ),
-			postContentEmpty: isEditedPostContentEmpty( state, siteId, postId )
+			title: getEditedPostValue( state, siteId, postId, 'title' )
 		};
 	},
 	( dispatch ) => {

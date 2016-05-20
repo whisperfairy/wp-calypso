9ce5e7763a3685ed8199a8ee5963bcf750d499a2
Giuseppe Gurgone <ggurgone@gmail.com>
2016-05-20 12:34:53 +0200

    DevDocs: Use DocsExample component and Components Usage Stats

--- client/components/global-notices/docs/example.jsx
+++ client/components/global-notices/docs/example.jsx
@@ -8,6 +8,7 @@ import React from 'react';
  */
 import Button from 'components/button';
 import ButtonGroup from 'components/button-group';
+import DocsExample from 'components/docs-example';
 import notices from 'notices';
 
 function showSuccessNotice() {
@@ -27,15 +28,18 @@ function showWarningNotice() {
 }
 
 const GlobalNotices = () => (
-	<div>
-		<h2>Global Notices</h2>
+	<DocsExample
+		title="Global Notices"
+		url="#"
+		componentUsageStats={ this.props.componentUsageStats }
+	>
 		<ButtonGroup>
 			<Button onClick={ showSuccessNotice }>Show success notice</Button>
 			<Button onClick={ showErrorNotice }>Show error notice</Button>
 			<Button onClick={ showInfoNotice }>Show info notice</Button>
 			<Button onClick={ showWarningNotice }>Show warning notice</Button>
 		</ButtonGroup>
-	</div>
+	</DocsExample>
 );
 GlobalNotices.displayName = 'GlobalNotices';
 

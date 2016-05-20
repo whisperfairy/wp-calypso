94105cad8a6d16aeb0c28f6720e607e1af8f636b
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-05-20 20:14:35 -0300

    Replaced i18n mixin with localize component

--- client/my-sites/upgrades/checkout-thank-you/personal-plan-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/personal-plan-details.jsx
@@ -7,12 +7,12 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import CustomDomainPurchaseDetail from './custom-domain-purchase-detail';
-import i18n from 'lib/mixins/i18n';
+import localize from 'lib/mixins/i18n/localize';
 import { isPersonal } from 'lib/products-values';
+import CustomDomainPurchaseDetail from './custom-domain-purchase-detail';
 import PurchaseDetail from 'components/purchase-detail';
 
-const PersonalPlanDetails = ( { selectedSite, sitePlans } ) => {
+const PersonalPlanDetails = ( { translate, selectedSite, sitePlans } ) => {
 	const plan = find( sitePlans.data, isPersonal );
 
 	return (
@@ -21,13 +21,11 @@ const PersonalPlanDetails = ( { selectedSite, sitePlans } ) => {
 
 			<PurchaseDetail
 				icon="speaker"
-				title={ i18n.translate( 'No Ads' ) }
-				description={
-					i18n.translate(
-						'Personal plan automatically removes all Ads from your site. ' +
-						'Now your visitors can enjoy your great content without distractions!'
-					)
-				}
+				title={ translate( 'No Ads' ) }
+				description={ translate(
+					'Personal plan automatically removes all Ads from your site. ' +
+					'Now your visitors can enjoy your great content without distractions!'
+				)	}
 			/>
 		</div>
 	);
@@ -41,4 +39,4 @@ PersonalPlanDetails.propTypes = {
 	sitePlans: React.PropTypes.object.isRequired
 };
 
-export default PersonalPlanDetails;
+export default localize( PersonalPlanDetails );

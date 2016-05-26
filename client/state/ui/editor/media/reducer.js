4bd000365bce88dee32307d9b1ac3b22a112b167
Andrew Duthie <andrew@andrewduthie.com>
2016-05-26 00:57:40 -0400

    State: Remove unused media state actions and reducer

--- client/state/ui/editor/media/reducer.js
+++ client/state/ui/editor/media/reducer.js
@@ -7,20 +7,7 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import imageEditor from './imageEditor/reducer';
-import {
-	EDITOR_MEDIA_EDIT_ITEM_SET
-} from 'state/action-types';
-
-export function editItem( state = null, action ) {
-	switch ( action.type ) {
-		case EDITOR_MEDIA_EDIT_ITEM_SET:
-			return action.item || null;
-	}
-
-	return state;
-}
 
 export default combineReducers( {
-	editItem,
 	imageEditor
 } );
